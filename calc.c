c28ee0ee38d3bb7c4580e9dfdb390526d03d2b70
Anderson <anderson.higo2@gmail.com>
2021-07-09 23:21:29 -0300

    Factorial

--- calc.c
+++ calc.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
 
 void division(float one, float two);
+void modulo(float one, float two);
+int	ft_recursive_factorial(int nb);
 
-int main(){
+int main()
+{
     char operation;
     float one;
     float two;
 
-    printf("Enter the operator (+ - * / %%): ");
+    printf("Enter the operator (+ - * / %% !): ");
     scanf("%c",&operation);
 
+    if(operation == '!')
+    {
+    printf("Enter number: ");
+    scanf("%e",&one);
+    printf("%2f! = %d\n", one, ft_recursive_factorial(one));
+    }
+    else
+    {
     printf("Enter two numbers(!!space key): ");
     scanf("%e %e",&one, &two);
+    }
     
     if(operation == '+')
     {
@@ -29,10 +41,15 @@ int main(){
     {
         division(one, two);
     }
+    else if(operation == '%')
+    {
+        modulo(one, two);
+    }
     return 0;
 }
 
-void division(float one, float two){
+void division(float one, float two)
+{
     if ((int)two == 0)
     {
         printf("divide by zero error not possible...");
@@ -41,4 +58,26 @@ void division(float one, float two){
     {
         printf("%f / %f = %f\n", one, two,(one / two));
     }
+}
+
+void modulo(float one, float two)
+{
+    if ((int)two == 0)
+    {
+        printf("divide by zero error not possible...");
+    }
+    else
+    {
+        printf("%f %% %f = %d\n", one, two,((int)one % (int)two));
+    }
+}
+
+int	ft_recursive_factorial(int nb)
+{
+	if (nb < 0)
+		return (0);
+	else if (nb == 0)
+		return (1);
+	else
+		return (nb * ft_recursive_factorial(nb - 1));
 }
